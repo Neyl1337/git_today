6b7ab3212bf166dd7d787f6f9a2a05abd3412105
Neyl <94065113+Neyl1337@users.noreply.github.com>
2025-02-24 10:49:54 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,39 +1,25 @@
 <?php
 
-//alapértelmezett menüpont beállitása
-$menupont=1;
+$menupont = 1;
 
-//Ellenőrizzük, hogy van-e GET paraméter, és ha igen, beállitjuk a menüpont értékét
-if(isset($_GET['m']))
-{
-    $menupont=$_GET['m'];
+if(isset($_GET['m'])) {
+    $menupont = $_GET['m'];
 }
 
-//menü tömb létrehozása
-//Többdimenziójú asszociatív tömb, amely minden menüponthoz egy tömböt tárol
-//Minden menüpontnak van egy ID-a, neve és címe
-
-$menu=array()
-{
+$menu = array(
     array("id" => 1, "nev" => "elso", "title" => "Első"),
     array("id" => 2, "nev" => "masodik", "title" => "Második"),
     array("id" => 3, "nev" => "harmadik", "title" => "Harmadik")
-};
+);
 
-//Tartalom tömb létrehozása
-//Szintén többdimenziós asszociatív tömb, mely a menüpontokhoz tartozó tartalmat 
-//tárolja
-//Minden elem egy egy menu_ID-jához kapcsolodik, valamint tartalmaz egy címet
-//és egy szöveget
-$tartalom=array()
-{
-    array("menu_id" =>1, "cim" => "Első első", "tartalom" => "tartalom első"),
-    array("menu_id" =>1, "cim" => "Első második", "tartalom" => "tartalom második"),
-    array("menu_id" =>2, "cim" => "Második első", "tartalom" => "tartalom első"),
-    array("menu_id" =>2, "cim" => "Második második", "tartalom" => "tartalom második"),
-    array("menu_id" =>3, "cim" => "Harmadik első", "tartalom" => "tartalom első"),
-    array("menu_id" =>1, "cim" => "Első harmadik", "tartalom" => "tartalom harmadik")
-};
+$tartalom = array(
+    array("menu_id" => 1, "cim" => "Első első", "tartalom" => "tartalom első"),
+    array("menu_id" => 1, "cim" => "Első második", "tartalom" => "tartalom második"),
+    array("menu_id" => 2, "cim" => "Második első", "tartalom" => "tartalom első"),
+    array("menu_id" => 2, "cim" => "Második második", "tartalom" => "tartalom második"),
+    array("menu_id" => 3, "cim" => "Harmadik első", "tartalom" => "tartalom első"),
+    array("menu_id" => 1, "cim" => "Első harmadik", "tartalom" => "tartalom harmadik")
+);
 
 ?>
 
@@ -42,9 +28,27 @@ $tartalom=array()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php print $menu[$menupont-1]["title"]; ?></title>
+    <title><?php print $menu[$menupont - 1]["title"]; ?></title>
 </head>
 <body>
-    
+    <?php
+    for ($i = 0; $i < count($menu); $i++) { 
+        $active = "";
+        if($menu[$i]["id"] == $menupont) {
+            $active = "active";
+        }
+        print "<a class=\"" . $active . "\" href=\"?m=" . $menu[$i]["id"] . "\">" 
+        . $menu[$i]["nev"] . "</a> ";
+    }
+    ?>
+    <br><br><br><br>
+    <?php 
+    for ($i = 0; $i < count($tartalom); $i++) { 
+        if ($tartalom[$i]["menu_id"] == $menupont) {
+            print "<h2>" . $tartalom[$i]["cim"] . "</h2>";
+            print "<p>" . $tartalom[$i]["tartalom"] . "</p>";
+        }
+    }
+    ?>
 </body>
 </html>
